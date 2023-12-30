c3754a4035127fe915893909bc96691044e80182
southsidemusa <97992584+southsidemusa@users.noreply.github.com>
2023-12-30 12:49:56 -0800

    Create calculator.py
    This code will generate a random password of a specified length using a combination of uppercase and lowercase letters, digits, and punctuation.

---
+++ calculator.py
@@ -0,0 +1,10 @@
+import string
+import random
+
+def generate_password(length):
+    all_characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(all_characters) for i in range(length))
+    return password
+
+# Generate a 12-character random password
+print(generate_password(12))
